d7756fe31864b3adf2944526a4f132fd305cc51f
Gaston Fuentes <gastonandres.fuentes@gmail.com>
2024-02-21 18:34:31 -0300

    third commit

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,29 +1,36 @@
-import {createContext, useState, useContext} from 'react'
-import PropTypes from 'prop-types';
-import { registerRequest } from "../api/auth";
+import { createContext, useState, useContext } from "react";
+import PropTypes from "prop-types";
+import { registerRquest } from "../api/auth";
 
 export const AuthContext = createContext();
+// eslint-disable-next-line react-refresh/only-export-components
 export const useAuth = () => {
-    const context = useContext(AuthContext);
-    if(!context){
-        throw new Error("useAuth must be a used within an AuthProvider");
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be a used within an AuthProvider");
+  }
+  return context;
+};
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const signup = async (user) => {
+    try {
+      const res = await registerRquest(user);
+      console.log(res.data);
+      setUser(res.data);
+      setIsAuthenticated(true);
+    } catch (error) {
+      console.log(error);
     }
-    return context;
-}
-export const AuthProvider = ({children}) => {
-    const [user, setUser] = useState(null);
-    const signup = async (user) => {
-        const res = await registerRequest(user);
-        console.log(res.data);
-        setUser(res.data);
-    }
-    return (
-        <AuthContext.Provider value={{signup, user}}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+  };
+  return (
+    <AuthContext.Provider value={{ signup, user, isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 AuthProvider.propTypes = {
-    children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 };
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,25 +1,45 @@
-import { useForm } from 'react-hook-form'
-import {useAuth} from '../context/AuthContext'
+import { useForm } from "react-hook-form";
+import { useAuth } from "../context/AuthContext"
+import { useEffect } from "react";
+import {useNavigate} from 'react-router-dom'
 
 function RegisterPage() {
+  const { register, handleSubmit } = useForm();
+  const { signup, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
 
-    const { register, handleSubmit } = useForm()
-    const {signup, user} = useAuth();
-    console.log(user);
-    const onSubmit = handleSubmit(async values => {
-        signup(values)
-    })
+  useEffect(() => {
+    if(isAuthenticated) navigate("/tasks");
+  }, [isAuthenticated, navigate]);
+  const onSubmit = handleSubmit(async (values) => {
+    signup(values);
+  });
 
-    return (
-        <div className="bg-zinc-800 max-w-md p-10 rounded-md">
-            <form onSubmit={onSubmit}>
-                <input type="text" {...register("username", { required: true })} placeholder="Username" className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" />
-                <input type="email" {...register("email", { required: true })} placeholder="Email" className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" />
-                <input type="password" {...register("password", { required: true })} placeholder="Password" className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" />
-                <button type="submit">Register</button>
-            </form>
-        </div >
-    )
+  return (
+    <div className="bg-zinc-800 max-w-md p-10 rounded-md">
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          {...register("username", { required: true })}
+          placeholder="Username"
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+        />
+        <input
+          type="email"
+          {...register("email", { required: true })}
+          placeholder="Email"
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+        />
+        <input
+          type="password"
+          {...register("password", { required: true })}
+          placeholder="Password"
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+        />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
